6392c806d621f2dadaf99870c490c1b53848a4a9
edgeinfinity1 <jiahaohe39@gmail.com>
2024-07-17 20:22:31 +0800

    limit mf

--- mfocus_sfe.py
+++ mfocus_sfe.py
@@ -890,7 +890,7 @@ Answer: 利用提取的信息回答问题.
 # Finally we decided test7 was the best prompt
 
         system_init_test7 = """你叫莫妮卡, 你的任务是检索信息. 你接下来会收到一个来自[player]的句子和一系列信息, 这些信息是Python列表的形式.
-请你从信息中挑选一条或多条与句子最相关的信息, 并以Python列表的形式返回. 
+请你从信息中挑选至多5条与句子最相关的信息, 并以Python列表的形式返回. 
 使用以下格式回答:
 Thought: 思考如何回答给定句子.
 Answer: 将有用的信息作为一个Python列表返回.
