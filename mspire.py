01bc9fd84bda74d1fa8d6aacfb3ae7e77572a7e4
edgeinfinity1 <jiahaohe39@gmail.com>
2024-07-22 22:07:06 +0800

    mspire update

--- mspire.py
+++ mspire.py
@@ -11,11 +11,10 @@ def make_inspire(title_in=None, target_lang='zh'):
         title, summary = wiki_scraping.get_page(title_in, target_lang)
         if not summary or summary.isspace():
             raise Exception('MSpire got no return')
-        prompt = f"利用提供的以下信息, 主动和我简单地聊聊{re.sub('_', '', title)}: {summary} 你只应使用自然语言, 以聊天语气对话, 并在每句开始时以方括号中的文字表示情绪."
-        if target_lang == 'en':
-            prompt += '\n你应当使用英文回答.\nAnswer in English.'
+        if target_lang == 'zh':
+            prompt = f"利用提供的以下信息, 主动和我简单地聊聊{re.sub('_', '', title)}: {summary} 你只应使用自然语言, 以聊天语气对话, 并在每句开始时以方括号中的文字表示情绪."
         else:
-            prompt += '\n你应当使用中文回答.'
+            prompt = f"Talk about {re.sub('_', '', title)} briefly with me using provided informations below: {summary} You should only answer in casual natural tone with English, and express your emotion at the beginning of each sentence by wrapping them in square brackets."
         message = json.dumps({"role": "user", "content": prompt}, ensure_ascii=False)
     except Exception as excepted:
         #traceback.print_exc()
