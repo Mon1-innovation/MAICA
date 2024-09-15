fed3bcbaf5b45c2e5d1dd107eb603316baed7126
edgeinfinity1 <jiahaohe39@gmail.com>
2024-09-16 00:23:45 +0800

    tolerance

--- persistent_extraction.py
+++ persistent_extraction.py
@@ -6,17 +6,17 @@ def read_from_sf(userid, chat_session_num, key):
             pass
     except:
         chat_session_num = 1
-    with open(f"persistents/{userid}_{chat_session_num}.json", 'r', encoding= 'utf-8') as savefile:
-        try:
+    try:
+        with open(f"persistents/{userid}_{chat_session_num}.json", 'r', encoding= 'utf-8') as savefile:
             sf_content = json.loads(savefile.read())
             sf_item = sf_content[key]
             if sf_item == None:
                 raise Exception('Marked as null so returning a failure')
             success = True
             return success, None, sf_item
-        except Exception as excepted:
-            success = False
-            return success, excepted, None
+    except Exception as excepted:
+        success = False
+        return success, excepted, None
 
 if __name__ == "__main__":
     print(read_from_sf(23, 1, "mas_geolocation"))
