905e6e0aec85549b42d0b494c9dd3dbb25a4732a
edgeinfinity1 <jiahaohe39@gmail.com>
2025-01-25 13:49:58 +0800

    waitress

--- maica_http.py
+++ maica_http.py
@@ -8,7 +8,9 @@ import functools
 import traceback
 import maica_ws
 from gevent import pywsgi
-from gunicorn.app.wsgiapp import WSGIApplication
+from waitress import serve
+from paste.translogger import TransLogger
+# from gunicorn.app.wsgiapp import WSGIApplication
 from Crypto.Random import random as CRANDOM
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
@@ -455,20 +457,20 @@ def seri_message(message):
         message_new.append(line_new)
     return message_new
 
-class StandaloneApplication(WSGIApplication):
-    def __init__(self, app_uri, options=None):
-        self.options = options or {}
-        self.app_uri = app_uri
-        super().__init__()
+# class StandaloneApplication(WSGIApplication):
+#     def __init__(self, app_uri, options=None):
+#         self.options = options or {}
+#         self.app_uri = app_uri
+#         super().__init__()
 
-    def load_config(self):
-        config = {
-            key: value
-            for key, value in self.options.items()
-            if key in self.cfg.settings and value is not None
-        }
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
+#     def load_config(self):
+#         config = {
+#             key: value
+#             for key, value in self.options.items()
+#             if key in self.cfg.settings and value is not None
+#         }
+#         for key, value in config.items():
+#             self.cfg.set(key.lower(), value)
 
 def run_http():
     #from gevent import monkey
@@ -495,15 +497,16 @@ def run_http():
     #     port= 6000,
     #     debug=False
     # )
-    server_thread = pywsgi.WSGIServer(('0.0.0.0', 6000), app)
+    # server_thread = pywsgi.WSGIServer(('0.0.0.0', 6000), app)
     # options = {
     #     "bind": "0.0.0.0:6000",
     #     "workers": 1,
         
     # }
     # StandaloneApplication("maica_http:app", options).run()
+    serve(TransLogger(app), host='0.0.0.0', port='6000')
     print('HTTP server started!')
-    server_thread.serve_forever()
+    # server_thread.serve_forever()
 
 if __name__ == '__main__':
     run_http()
