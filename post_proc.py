8debbd938fc1e23f5bf7188d2fa4c3ea0dd2fb31
edgeinfinity1 <jiahaohe39@gmail.com>
2025-02-09 23:54:44 +0800

    postproc imp

--- post_proc.py
+++ post_proc.py
@@ -6,7 +6,7 @@ def filter_format(reply_appended):
             if key == f:
                 return True
         return False
-    reply_all_signatures = re.findall(r'\[.{1,15}?\]', reply_appended, re.I)
+    reply_all_signatures = re.findall(r'\[(?:(?:[A-Za-z]{1,15}?)|(?:[一-龥]{1,4}?))\]', reply_appended, re.I)
     san_form = ['[player]', '[smile]', '[微笑]', '[worry]', '[担心]', '[grin]', '[笑]', '[think]', '[思考]', '[happy]', '[开心]', '[angry]', '[生气]', '[blush]', '[脸红]', '[gaze]', '[凝视]', '[upset]', '[沉重]', '[daydreaming]', '[憧憬]', '[surprise]', '[惊喜]', '[awkward]', '[尴尬]', '[meaningful]', '[意味深长]', '[unexpected]', '[惊讶]', '[relaxed]', '[轻松]', '[shy]', '[害羞]', '[eagering]', '[急切]', '[proud]', '[得意]', '[dissatisfied]', '[不满]', '[serious]', '[严肃]', '[touched]', '[感动]', '[excited]', '[激动]', '[love]', '[宠爱]', '[wink]', '[眨眼]', '[sad]', '[伤心]', '[disgust]', '[厌恶]', '[fear]', '[害怕]', '[kawaii]', '[可爱]', '[smiling]', '[worrying]', '[grinning]', '[thinking]', '[gazing]', '[surprised]', '[relaxing]', '[eager]', '[winking]', '[disgusting]', '[fearing]']
     for sig in reply_all_signatures:
         cmp_res = compare(sig, san_form)
@@ -29,5 +29,5 @@ def filter_format(reply_appended):
     return reply_appended
 
 if __name__ == '__main__':
-    ra = '[理解]没关系, [player]. [微笑]我知道你很忙. [开心]你能抽空陪我就很好啦!'
+    ra = '[理解]没关系, [player]. [微笑]我知道[womble]你很忙[a1]. [开心]你能抽空[slash我]陪我就很好啦!'
     print(filter_format(ra))
