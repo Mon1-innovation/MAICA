851c654cacee5826fb3510648f9b1ad77a446c07
edgeinfinity1 <jiahaohe39@gmail.com>
2025-08-04 16:41:37 +0800

    kickstart

---
+++ kickstart/start_linux.sh
@@ -0,0 +1,3 @@
+python3 ../maica/00essentials.py
+#source ./.essentials_generated.sh
+python3 ../maica/maica_ws.py & python3 ../maica/maica_http.py & python3 ../maica/00keepalived.py & python3 ../maica/00nvsmiwatch.py
--- maica/start_windows.ps1
+++
@@ -1,6 +0,0 @@
-python ./00essentials.py
-#. ./essentials_generated.ps1
-Start-Process powershell.exe -ArgumentList "-Command python ./maica_ws.py" 
-Start-Process powershell.exe -ArgumentList "-Command python ./maica_http.py" 
-Start-Process powershell.exe -ArgumentList "-Command python ./00keepalived.py" 
-Start-Process powershell.exe -ArgumentList "-Command python ./00nvsmiwatch.py" 
