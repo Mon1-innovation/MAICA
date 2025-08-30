d945cd66732c7d8446f2394d607d63ebef83715b
edgeinfinity1 <jiahaohe39@gmail.com>
2025-08-30 14:06:14 +0800

    schedule sigint

--- maica/common_schedule.py
+++ maica/common_schedule.py
@@ -7,7 +7,6 @@ from maica_utils import *
 
 async def rotate_cache(maica_pool: DbPoolCoroutine):
     """Always provide a pool in production deployment!"""
-    maica_pool = default(maica_pool, await ConnUtils.maica_pool())
     keep_time = load_env('ROTATE_MSCACHE')
     if int(keep_time):
         timestamp = datetime.datetime.now()
@@ -18,14 +17,21 @@ async def rotate_cache(maica_pool: DbPoolCoroutine):
             if row[1] + datetime.timedelta(hours=int(keep_time)) <= timestamp:
                 await maica_pool.query_modify(sql_expression_2, (row[0]))
 
-def wrap_rotate_cache(maica_pool=None):
+def wrap_rotate_cache(maica_pool):
     asyncio.run(rotate_cache(maica_pool))
 
 async def schedule_rotate_cache(**kwargs):
+    maica_created = False
+    if kwargs.get('maica_pool'):
+        maica_pool = kwargs.get('maica_pool')
+    else:
+        maica_pool = await ConnUtils.maica_pool()
+        maica_created = True
+
     await messenger(info="MAICA scheduler started!", type=MsgType.SYS)
     if load_env('ROTATE_MSCACHE') != '0':
         await messenger()
-        schedule.every().day.at("04:00").do(wrap_rotate_cache, **kwargs)
+        schedule.every().day.at("04:00").do(wrap_rotate_cache, maica_pool=maica_pool)
     try:
         while True:
             schedule.run_pending()
@@ -33,5 +39,8 @@ async def schedule_rotate_cache(**kwargs):
     except BaseException:
         pass
     finally:
+        if maica_created:
+            await maica_pool.close()
+            
         await messenger(info='\n', type=MsgType.PLAIN)
         await messenger(info="MAICA scheduler stopped!", type=MsgType.SYS)
