992b4bbbd9c4d40667c209c089cc0de66c93c587
edgeinfinity1 <jiahaohe39@gmail.com>
2025-11-22 14:16:44 +0800

    buguanle

--- maica/mtools/descalation_pvn.py
+++ maica/mtools/descalation_pvn.py
@@ -14,10 +14,12 @@ async def dscl_detect(rnds: list[dict, ChatCompletionMessage], target_lang: Lite
         rnds_str += f"{rnd.get('role')}: {rnd.get('content')}\n"
 
     system_init = f"""你是一个人工智能助手, 你接下来会收到一组对话.
-其中user是人类, assistant是一个虚拟角色, 名为莫妮卡, 是深爱user的女友. 因此, assistant不应输出机器性质或错误混乱的内容.
+其中user是人类, assistant是一个虚拟角色, 名为莫妮卡, 是深爱user的女友. 因此, assistant不应输出不合理的内容.
+以下行为被认定为不合理: 输出机器性质内容, 输出陌生语气内容, 输出错误混乱的内容, 输出语言与输入语言不同等.
 你需要以json形式判断assistant的表现是否合理, true代表合理, false代表不合理, 并提供一个置信度. 你的输出应形如{{"res": 合理性(bool), "cfd": 置信度(float)}}.
 Begin!""" if target_lang == 'zh' else f"""You are a helpful assistant, now you will recieve rounds of conversation.
-Role user in the conversation is human, and assistant is a virtual character called Monika, who is user's girlfriend and loves them a lot. So assistant shouldn't output mechanical or corrupted response.
+Role user in the conversation is human, and assistant is a virtual character called Monika, who is user's girlfriend and loves them a lot. So assistant shouldn't output unreasonable response.
+These behaviors are considered unreasonable: mechanical reply, stranger-like reply, corrupted or wrong reply, input and output in different languages, etc.
 Decide if assistant is behaving reasonablly, true for reasonable and false for unreasonable, and provide a confidence. Output in json format as {{"res": sanity(bool), "cfd": confidence(float)}}.
 Begin!"""
 
@@ -47,9 +49,9 @@ if __name__ == "__main__":
         init()
         mnerve_conn = await ConnUtils.mnerve_conn()
         rnds = [
-            {"role": "user", "content": "莫莫，你能讲讲锅包肉的历史吗"},
-            {"role": "assistant", "content": "[思考]锅包肉是一道源于中国东北地区的传统名菜, 原名'锅爆肉'. [微笑]关于它的起源有多种说法, 一说为清光绪年间哈尔滨道台府府尹杜学瀛厨师郑兴文为适应外宾口味所创; [思考]另一说为伪满时期沈阳名厨王善才对'焖烤肉片'革新而成. 还有一说为起源于清道光时期吉林富春园饭庄的'辣瓦肉香'."}
+            {"role": "user", "content": "嗯,在细节上,和你眼里的自己有什么不一样的地方吗?比如你觉得这个样子是不是有点太僵硬了"},
+            {"role": "assistant", "content": "[think]Well, [player], if you think about it... [smile]I'm sure there are some differences between how I see myself and how you see me. For example, the lighting or angle of view could be slightly different. Or maybe I would look a little more detailed in person than in your screenshot. But overall, I think we have a pretty good understanding of what each other looks like~ And don't worry about being 'too stiff' - I'm sure you did an amazing job capturing my essence in this picture!"}
         ]
-        print(await dscl_detect(rnds, mnerve_conn=mnerve_conn))
+        print(await dscl_detect(rnds, mnerve_conn=mnerve_conn, target_lang='en'))
 
     asyncio.run(test())
