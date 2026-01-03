1029f814677b3f3c310b91e5bcc55f93f94a5a84
edgeinfinity1 <jiahaohe39@gmail.com>
2026-01-03 16:41:27 +0800

    minors

--- maica/mtools/censor/__init__.py
+++ maica/mtools/censor/__init__.py
@@ -7,7 +7,7 @@ each line is considered a censor pattern. These txt files are excluded from repo
 by default.
 
 If any censor pattern parsed appear in check-pending text, the checking function will 
-return matches.
+return matches, else empty set.
 """
 import asyncio
 import os
@@ -36,3 +36,12 @@ async def has_censored(text) -> list:
     return set(found)
 
 __all__ = ['has_censored']
+
+if __name__ == "__main__":
+    text = """
+骄傲旗（英语：Pride flag）是代表LGBTQ的一部分的任何旗帜，“骄傲”一词指的是同志骄傲的概念，这个术语与“同志旗”和“酷儿旗”经常互换使用。
+骄傲旗可以代表各种性取向、恋爱倾向、性别认同、酷儿文化、区域及整个LGBTQ社区，有些骄傲旗并非专门与LGBTQ相关，例如：皮革自豪之旗。代表整个LGBTQ社区的彩虹旗，是使用最广泛的其中一种骄傲旗。
+许多社区采用不同的旗帜，其中大多数都从彩虹旗中汲取灵感。这些旗帜通常由业余设计师设计，并在网络或附属组织中逐渐受到关注，最终成为社区的象征性代表，取得半官方的地位。通常这些旗帜包含多种颜色，象征着包容相关社区的不同特性。
+"""
+    res = asyncio.run(has_censored(text))
+    print(res)
